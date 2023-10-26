95d532c3ccaa089623961074c603d5f122610a4f
EduEduaa <107058483+EduEduaa@users.noreply.github.com>
2023-10-25 22:50:48 -0300

    wena perro
    chaooo

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,14 +1,14 @@
 export const environment = {
   firebase: {
-    projectId: 'tellevo-4bb1b',
-    appId: '1:148484254873:web:9f1c4ff2064ce420731c08',
-    storageBucket: 'tellevo-4bb1b.appspot.com',
-    apiKey: 'AIzaSyCDacxvBbmetqLlGTwYMX_W9n_EpYPccEE',
-    authDomain: 'tellevo-4bb1b.firebaseapp.com',
-    messagingSenderId: '148484254873',
-    measurementId: 'G-JPNKJ5N4X8',
+    projectId: 'tellevo-app-8e068',
+    appId: '1:975880927614:web:1455bc2a923fb0b1659b2f',
+    storageBucket: 'tellevo-app-8e068.appspot.com',
+    apiKey: 'AIzaSyDO1IsIGokumwo4QHl4JaDVXTq7UYkSRxU',
+    authDomain: 'tellevo-app-8e068.firebaseapp.com',
+    messagingSenderId: '975880927614',
   },
-  production: true,
+
+  production: true
   
 
 };
